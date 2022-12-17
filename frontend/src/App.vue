e2a01aa76f64522e58c24e7ab8badae9d00def5b
Obl4ki <lukaszoblak33@gmail.com>
2022-12-17 18:01:53 +0100

    Add login form view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
+import LoginForm from "./components/LoginForm.vue";
 </script>
 
 <template>
-<h1>Works</h1>
+  <div class="centered">
+    <LoginForm />
+  </div>
 </template>
 
 <style scoped>
+.centered {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
 
 </style>
---
+++ frontend/src/components/LoginForm.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts"></script>
+
+<template>
+  <form id="login">
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/1000px-Adidas_Logo.svg.png"
+    /><br />
+    <label for="email">Email</label><br />
+    <input type="text" id="email" placeholder="Wpisz Email" /><br /><br />
+    <label>Hasło</label><br />
+    <input type="text" id="password" placeholder="Wpisz hasło" /><br />
+    <a href="">Zapomniałem hasła</a><br />
+    <button type="button">Dalej</button>
+  </form>
+</template>
+
+<style scoped lang="scss">
+$border-color: #aeaeae;
+$font-color: #0082fb;
+$input-color: #0082fbc9;
+$hover-color: #005fb8;
+
+form#login {
+  margin: auto;
+  padding: 50px;
+  text-align: center;
+  width: 400px;
+  border: 1px solid $border-color;
+  border-radius: 23px;
+  font-family: Arial;
+
+  img {
+    width: 100px;
+    height: 100px;
+    margin: 30px;
+    margin-top: 0px;
+  }
+
+  label {
+    font-size: 22px;
+    font-weight: bold;
+    color: $font-color;
+  }
+
+  input {
+    border-radius: 4px;
+    padding: 12px;
+    border: 3px solid $input-color;
+    margin: 8px;
+    margin-bottom: 1px;
+    width: 300px;
+    box-shadow: 0 6px 2px -2px rgba(128, 128, 128, 0.473);
+  }
+
+  a {
+    color: $font-color;
+    font-size: 12px;
+    position: relative;
+    right: 100px;
+    text-decoration: none;
+  }
+
+  button {
+    color: white;
+    font-size: 18px;
+    font-weight: bold;
+    background-color: $font-color;
+    padding: 12px;
+    padding-left: 20px;
+    padding-right: 20px;
+    margin-top: 50px;
+    margin-bottom: 0px;
+    border-radius: 10px;
+    border-style: none;
+    position: relative;
+    left: 120px;
+    transition-duration: 0.1s;
+  }
+
+  button:hover {
+    background-color: $hover-color;
+  }
+}
+</style>
