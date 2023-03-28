e2aeb44170d190d942d78962b6a13e1c8bfea31e
Michał Szczepaniak <miher13@wp.pl>
2023-03-28 12:21:30 +0200

    Add ModelMapping for DTO

--- Inwentaryzacja/src/main/java/Zut_IPZ/Inwentaryzacja/InwentaryzacjaApplication.java
+++ Inwentaryzacja/src/main/java/Zut_IPZ/Inwentaryzacja/InwentaryzacjaApplication.java
@@ -1,10 +1,17 @@
 package Zut_IPZ.Inwentaryzacja;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InwentaryzacjaApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(InwentaryzacjaApplication.class, args);
 	}
