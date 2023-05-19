e1d927d669cb61eef60134c6bdd9e9a64070c688
Obl4ki <lukaszoblak33@gmail.com>
2023-05-19 20:42:06 +0200

    Change register and login view logo image to box

--- frontend/src/components/RegistrationForm.vue
+++ frontend/src/components/RegistrationForm.vue
@@ -24,9 +24,7 @@ function passwordsAreTheSame(): boolean {
 
 <template>
   <form id="login">
-    <img
-      src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/1000px-Adidas_Logo.svg.png"
-    /><br />
+    <img src="box.png" /><br />
     <label for="email">Email</label><br />
     <input
       v-model="email"
