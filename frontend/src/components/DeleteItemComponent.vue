42978706a5a30639957946e5175217686c2ed077
Obl4ki <lukaszoblak33@gmail.com>
2023-05-31 00:45:53 +0200

    DeleteEntry View styling

--- frontend/src/components/DeleteItemComponent.vue
+++ frontend/src/components/DeleteItemComponent.vue
@@ -29,21 +29,56 @@ async function deleteItem() {
 </script>
 
 <template>
-  <div class="wrap">
-    <div class="delete-info">You are about to delete {{ selectedEntry?.item?.name }}</div>
+  <div class="wrap" v-if="selectedEntry">
+    <div class="delete-info dark">
+      You are about to delete {{ selectedEntry?.item?.name }}
+    </div>
+
     <form @submit.prevent="deleteItem" action="delete">
       <h3>Are you sure?</h3>
-      <button type="submit" class="confirm-delete">
+      <button type="submit" class="confirm-delete dark light-bg">
         Yes, delete this item.
       </button>
     </form>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+
+@mixin border-radius($radius) {
+  -webkit-border-radius: $radius;
+     -moz-border-radius: $radius;
+      -ms-border-radius: $radius;
+          border-radius: $radius;
+}
+
 .wrap {
   height: 80vh;
   background: white;
-  padding: 20px;
+  padding: 5vmin;
+}
+
+.delete-info {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+form {
+    width: 30vw;
+    height: 30vh;
+    margin: 0 auto;
+
+
+  h3 {
+    margin-top: 10px;
+    text-align: center;
+    width: 100%;
+  }
+  button {
+    @include border-radius(5px); 
+    margin-top: 10px;
+    width: 100%;
+    height: 2rem;
+  }
 }
 </style>
